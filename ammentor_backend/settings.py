a1df24ca379ccb1150a8a4aa0a79f52324195759
Naveen <naveensrinivas282@gmail.com>
2025-04-22 01:59:03 +0530

    Changed settings to handle static_root

--- ammentor_backend/settings.py
+++ ammentor_backend/settings.py
@@ -86,6 +86,10 @@ DATABASES = {
         "PORT": os.getenv("SQL_PORT"),
     }
 }
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
